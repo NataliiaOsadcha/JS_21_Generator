8b7ba5f9f3b3671d7be48a6e07584969a9519020
NataliiaOsadcha <nat.osadchaya@gmail.com>
2023-07-19 15:41:55 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+
+function Boo() {
+  let index = 0;
+  const values = [1, 2, 3];
+
+  console.log('start');
+
+  return {
+    next: function () {
+      if (index < values.length) {
+        return { value: values[index++], done: false };
+      } else {
+        console.log('finish');
+        return { done: true };
+      }
+    }
+  };
+}
+
+const iterator = Boo();
+console.log(iterator.next().value);
+console.log(iterator.next().done); 
